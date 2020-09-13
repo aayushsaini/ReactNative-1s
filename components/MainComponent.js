63c8ef4290f1c63f32b5a561fc19ac8fbc45fa4f
aayushsaini <aayush.r98@gmail.com>
2020-09-14 00:53:04 +0530

    Navigation Fix Patch

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,29 +1,70 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import DishDetial from './DishDetailComponent';
-import { View } from 'react-native';
+import Home from './HomeComponent';
+import Dishdetail from './DishDetailComponent';
+import { View, Platform } from 'react-native';
+import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+},
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
+
+const HomeNavigator = createStackNavigator({
+    Home: {screen: Home}
+},
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+    }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+})
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
 
     render() {
 
         return (
-            <View style={{flex:1}}>
-                <Menu dishes={this.state.dishes}
-                    onPress={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetial dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <View style={{ flex: 1 }}>
+                <MainNavigator />
             </View>
         );
     }
